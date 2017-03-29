c707930d1703d6372ecd05f7f80afb6af30c2be5
root <root@server.training.local>
2017-03-29 15:04:46 +0530

    ftp done.

--- OpenSource/FTP Server cfg/ftp1/check.sh
+++ OpenSource/FTP Server cfg/ftp1/check.sh
@@ -16,18 +16,17 @@ if [ $a -eq 0 ] && [ $b -eq 0 ] && [ $c -eq 0 ]; then
 	echo "Pass."
 	exit
 fi
-grep "soni" /tmp/exam/user_list_$ip &>> /tmp/exam/flogs
-a1=$? # 0 for pass
-grep "ankush" /tmp/exam/user_list_$ip &>> /tmp/exam/flogs
-a2=$? # 0 for pass
-grep "soni" /tmp/exam/ftpusers_$ip &>> /tmp/exam/flogs
-a3=$? # 1 for pass
-grep "ankush" /tmp/exam/ftpusers_$ip &>> /tmp/exam/flogs
-a4=$? # 1 for pass
-grep "userlist_deny=NO" /tmp/exam/vsftpd.conf_$ip &>> /tmp/exam/flogs
-a5=$? # 0 for pass
-if [ $a1 -eq 0 ] && [ $a2 -eq 0 ] && [ $a3 -eq 1 ] && [ $a4 -eq 1 ] && [ $a5 -eq 0 ]; then
-	echo "Pass."
-else 
-	echo "Fail."
+grep "#root" /tmp/exam/user_list_$ip &>> /tmp/exam/flogs
+a=$?
+grep "#root" /tmp/exam/ftpusers_$ip &>> /tmp/exam/flogs
+b=$?
+grep "root" /tmp/exam/user_list_$ip &>> /tmp/exam/flogs
+c=$?
+grep "root" /tmp/exam/ftpusers_$ip &>> /tmp/exam/flogs
+d=$?
+if [ $a -eq 0 ] || [ $c -eq 1 ] && [ $b -eq 0 ] || [ $d -eq 1 ]; then
+        echo "Pass."
+else
+        echo "Fail."
 fi
+
